bbba1857aa727b3d40942e7789c81968316904e7
T. E. Pickering <te.pickering@gmail.com>
2022-06-23 19:43:02 -0700

    flesh out and add redis support to tcs_logger

--- src/minicyclop/tcs_logger.py
+++ src/minicyclop/tcs_logger.py
@@ -12,8 +12,8 @@ from pathlib import Path
 
 from minicyclop.io import read_latest
 
-log = logging.getLogger('MiniCyclop')
-log.setLevel(logging.DEBUG)
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
 def main():
@@ -30,14 +30,40 @@ def main():
 
     log.info(f"Monitoring {args.filename} for seeing data...")
 
-    last_time = None
+    # initialize with existing file so stale data doesn't get sent
+    latest_data = read_latest(args.filename)
+    last_time = latest_data['obstime']
+
+    if 'REDISHOST' in os.environ:
+        redis_host = os.environ['REDISHOST']
+    else:
+        redis_host = 'redis.mmto.arizona.edu'
+
+    if 'REDISPORT' in os.environ:
+        redis_port = os.environ['REDISPORT']
+    else:
+        redis_port = 6379
+
+    if 'REDISPW' in os.environ:
+        redis_server = redis.StrictRedis(host=redis_host, port=redis_port, password=os.environ['REDISPW'], db=0)
+    else:
+        redis_server = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
 
     while True:
         latest_data = read_latest(args.filename)
-        if latest_data['obstime'] != last_time:
-            log.debug(f"New data found at {latest_data['obstime_str']}: seeing = {latest_data['seeing']}\"")
+        if latest_data['obstime'] > last_time:
+            log.info(f"New data found at {latest_data['obstime_str']}: seeing = {latest_data['seeing']}\"")
             last_time = latest_data['obstime']
+            latest_data['measurement_timestamp'] = latest_data['obstime_str']
+            for k in ['seeing', 'flux', 'r0', 'measurement_timestamp']:
+                redis_key = f"seeing_monitor_{k}"
+                try:
+                    redis_server.set(redis_key, latest_data[k])
+                    redis_server.publish(redis_key, latest_data[k])
+                except Exception as e:
+                    log.warning(f"Failed to set redis {redis_key} to {latest_data[k]}: {e}")
+
         else:
-            log.debug("Waiting for data...")
+            log.info("Waiting for data...")
 
         time.sleep(1)
